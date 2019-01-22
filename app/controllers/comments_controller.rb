c2dbb765142b41823ca0354d58ff34704abbc6dd
Guilherme Oliveira <guilherme.oliveira@codus.com.br>
2019-01-22 16:11:02 -0200

    Created a method for deciding if the comment is being created for a Post or a Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :find_commentable
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
+
+    @comment = @commentable.comments.new(comment_params)
 
       if @comment.save
         redirect_to @post
       else
-
         flash[:notice] = "You can't leave the comment in blank"
         redirect_to @post
       end
@@ -19,5 +19,10 @@ class CommentsController < ApplicationController
 
     def comment_params
       params.require(:comment).permit(:body)
+    end
+
+    def find_commentable
+      @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+      @commentable = Post.find_by_id(params[:post_id]) if params[:post_id]
     end
 end
